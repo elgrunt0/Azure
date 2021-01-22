2660ca06fbff21885c736f4e01eb684380613aa4
Grant <elgrunt0@users.noreply.github.com>
2021-01-22 16:43:48 +0000

    Install vscode commands

--- WVD/ImageCustomisation/1_Start-WVDCustomisation.ps1
+++ WVD/ImageCustomisation/1_Start-WVDCustomisation.ps1
@@ -1,2 +1,9 @@
 #Import Modules and then invoke next script.
 
+#Create temp folder
+New-Item -Path 'C:\temp' -ItemType Directory -Force | Out-Null
+
+
+#Install VSCode
+Invoke-WebRequest -Uri 'https://go.microsoft.com/fwlink/?Linkid=852157' -OutFile 'c:\temp\VScode.exe'
+Invoke-Expression -Command 'c:\temp\VScode.exe /verysilent'
